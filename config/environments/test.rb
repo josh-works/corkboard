cd9bba07a3dbcef3f252aca1e91d42e0805db19f
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-05-17 17:13:02 -0600

    initial set up
    installs rspec and other gems

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Corkboard::Application.configure do |config|
+    # Access to rack session
+    config.middleware.use RackSessionAccess::Middleware
+  end
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
