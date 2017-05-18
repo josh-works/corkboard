25b8d5d4fa07de82e622b643ddc43e22d894aa77
Riley Thompson <rileyt_2000@hotmail.com>
2017-05-18 11:46:54 -0600

    add base oauth config
    * add omniauth-facebook gem
    * add figaro gem
    * add omniauth.rb initialize config
    * add app_id/secret in application.yml
    * add new view for /register
    * add user controller for new view

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
 
   has_many :user_roles
   has_many :roles, through: :user_roles
+
+  def self.from_omniauth(auth_info)
+  user = where(uid: auth_info[:uid]).first_or_initialize do |new_user|
+    new_user.uid                = auth_info.uid
+    new_user.name               = auth_info.info.name
+    new_user.token        = auth_info.credentials.token
+  end
+ end
 end
