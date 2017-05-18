818a7d814822aaa2a64d706b81e926dd9e23a898
Riley Thompson <rileyt_2000@hotmail.com>
2017-05-18 15:55:39 -0600

    fix omniauth_info variable usage in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
    user = User.from_omniauth(omniauth_info)
 
    if user && user.new_record?
-    render :template => "oauth/new", locals: {oauth_info: request.env["omniauth.auth"], user: User.new, name: User.name_from_oauth(request.env["omniauth.auth"])}
+    render :template => "oauth/new", locals: {oauth_info: omniauth_info, user: User.new, name: User.name_from_oauth(omniauth_info)}
   else
     session[:user_id] = user.id
     redirect_to root_path
