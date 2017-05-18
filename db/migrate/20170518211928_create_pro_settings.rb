c141f55f101519ebdc1c4e5f4637f803f3d0b12f
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-05-18 15:38:00 -0600

    stuff

--- db/migrate/20170518211928_create_pro_settings.rb
+++ db/migrate/20170518211928_create_pro_settings.rb
@@ -2,9 +2,9 @@ class CreateProSettings < ActiveRecord::Migration[5.0]
   def change
     create_table :pro_settings do |t|
       t.references :industry, foreign_key: true
-      t.references :pro, foreign_key: :user_id
+      t.references :user, foreign_key: true
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
