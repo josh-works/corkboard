5ba22998027bd40d069134bae4bfd098aef0efb3
Seth <seth19483@gmail.com>
2017-05-21 13:08:09 -0600

    Editting navbar
    extract logic from navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def login_button
+    if logged_in?
+      link_to 'Logout', logout_path, method: :delete
+    else
+      link_to 'Log In', login_path
+    end
+  end
+
+  def sign_up_button
+    if !logged_in?
+      link_to 'Sign Up', choose_account_path
+    end
+  end
 end
