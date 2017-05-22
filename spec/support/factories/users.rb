18c660b0d9a902032b1c90c3e7deabd412828415
Riley Thompson <rileyt_2000@hotmail.com>
2017-05-22 13:38:02 -0600

    add feature test for login

--- spec/support/factories/users.rb
+++ spec/support/factories/users.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     first_name Faker::Name.first_name
     last_name Faker::Name.last_name
     zipcode Faker::Address.zip_code
+    password 'hunter2'
     phone_number Faker::PhoneNumber.phone_number
     email Faker::Internet.email
     roles { [build(:customer)] }
