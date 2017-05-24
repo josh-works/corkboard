1c6d52f5f92b5366a14c564e1b15c43ceedeaa3f
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-05-24 00:33:32 -0600

    builds valid pro_service factory
    begins setup for pro sees projects spec

--- spec/features/pro/can_see_available_project_spec.rb
+++ spec/features/pro/can_see_available_project_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'a logged pro can see available projects that match its services' do
-  let(:service) { (create(:service)) }
+  let!(:industry)  { create(:industry, name: 'Home Improvement')}
+  let!(:category)  { create(:category, name: 'Lawn & Garden', industry: industry) }
+  let!(:service_1) { create(:service, category: category) }
+  let!(:service_2) { create(:service, name: 'Gardening', category: category) }
+  let!(:project_1) { create(:project, service: service_1) }
+  let!(:project_2) { create(:project, service: service_2) }
+  let!(:pro) { (create(:pro_user)) }
 
   it 'visits find new projects page and and sees a list of projects that match services' do
-
+    byebug
   end
 end
