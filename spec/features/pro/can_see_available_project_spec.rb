da470eeeb0e00b0cd7037d6cf707a36469f4fb0c
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-05-24 01:11:02 -0600

    changes order of model creation to take advantage of lazy loading

--- spec/features/pro/can_see_available_project_spec.rb
+++ spec/features/pro/can_see_available_project_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'a logged pro can see available projects that match its services' do
-  let!(:industry)  { create(:industry, name: 'Home Improvement')}
-  let!(:category)  { create(:category, name: 'Lawn & Garden', industry: industry) }
-  let!(:service_1) { create(:service, category: category) }
-  let!(:service_2) { create(:service, name: 'Gardening', category: category) }
-  let!(:project_1) { create(:project, service: service_1) }
-  let!(:project_2) { create(:project, service: service_2) }
-  let!(:pro_service) { create(:pro_service, service_ids: [service_1.id]) }
-  let!(:pro) { create(:pro_user, pro_service: pro_service) }
-  let(:project_1_email) { project_1.requester.email }
   let(:project_2_email) { project_2.requester.email }
+  let(:project_1_email) { project_1.requester.email }
+  let(:pro) { create(:pro_user, pro_service: pro_service) }
+  let(:pro_service) { create(:pro_service, service_ids: [service_1.id]) }
+  let(:project_1) { create(:project, service: service_1) }
+  let(:project_2) { create(:project, service: service_2) }
+  let(:service_1) { create(:service, category: category) }
+  let(:service_2) { create(:service, name: 'Gardening', category: category) }
+  let(:category)  { create(:category, name: 'Lawn & Garden', industry: industry) }
+  let(:industry)  { create(:industry, name: 'Home Improvement')}
 
-  xit 'visits find new projects page and and sees a list of projects that match services' do
+  it 'visits find new projects page and and sees a list of projects that match services' do
     page.set_rack_session(user_id: pro.id, authenticated: true)
-
+    byebug
     visit '/pro/dashboard'
 
     click_link 'Find Projects'
