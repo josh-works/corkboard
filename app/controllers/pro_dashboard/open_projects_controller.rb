9467ca8cb87461d9cd8cce19f277d40694540f3e
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-06-05 17:16:09 -0600

    cleaned up the geospatial search code but still having to hit API too frequent for good use

--- app/controllers/pro_dashboard/open_projects_controller.rb
+++ app/controllers/pro_dashboard/open_projects_controller.rb
@@ -1,22 +1,18 @@
 class ProDashboard::OpenProjectsController < ApplicationController
   attr_accessor :to_bid_on
   def index
-    #add in filter about open/closed status of project
     @pro = Pro.find(current_user.id)
     @pro_location = Geokit::Geocoders::GoogleGeocoder.geocode(@pro.zipcode)
     @open_projects = @pro.open_projects
-
     @to_bid_on = []
 
     @open_projects.each do |open_project|
-      request_location = open_project.zipcode
-      distance = @pro_location.distance_to(request_location)
+      distance = @pro_location.distance_to(open_project.zipcode)
+      require "pry"; binding.pry
       if distance < @pro.pro_service.radius.to_i
         @to_bid_on << open_project
-      else
       end
     end
-    # request_location = Geokit::Geocoders::GoogleGeocoder.geocode(@pro.zipcode)
   end
 
   def show
