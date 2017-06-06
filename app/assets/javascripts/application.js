e07ef9774415752cbe7c3563bd6fc1fe2a3ce70d
jk1dd <portajonk@gmail.com>
2017-06-06 14:51:55 -0600

    update jquery file upload requirmetns in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-fileupload
+//= require jquery-fileupload/basic
+//= require jquery-fileupload/vendor/tmpl
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
