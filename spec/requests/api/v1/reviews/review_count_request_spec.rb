4e6ea75bcfbc1ef3a1d0501a525ee7ee58d9ff30
Beth Knight <bethknight1234@gmail.com>
2017-06-06 13:57:59 -0600

    pro user fix factory girl

--- spec/requests/api/v1/reviews/review_count_request_spec.rb
+++ spec/requests/api/v1/reviews/review_count_request_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Reviews API" do
   it "shows total count of reviews per pro" do
+    pro = create(:pro_user)
+    review = create(:review, pro: pro)
+
     list = create_list(:review, 25)
 
-    require "pry"; binding.pry
 
     get "/api/v1/reviews/review_count"
 
