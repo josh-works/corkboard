a34fdc09ffbbf3a8145a3ec9afe975dbb99990f3
Beth Knight <bethknight1234@gmail.com>
2017-06-07 14:34:37 -0600

    create review partial on pro show

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,23 +7,9 @@ class Review < ApplicationRecord
       where(pro_id: pro).count
     end
 
-  def self.total_average(pro)
-    where(pro_id: pro).average(:rating).to_i
-  end
+    def self.total_average(pro)
+      where(pro_id: pro).average(:rating).to_i
+    end
 
-  def self.highest_rated(service)
-    Review.where(service_id: 261).pluck(:pro_id)
-      # find all the pro_ids associated with that service
-      #   .where(pro_id: pro).average(:rating).to_i
-      # attach average rating to each pro object
-      # rank them highest to lowest
-      # return highest
-  end
 
-  # def self.lowest_rated(category)
-  #   # find the category
-  #   # find all the pros associated with that category
-  #   # find total_average of each pro
-  #   # organize pros by lowest to highest
-  # end
 end
